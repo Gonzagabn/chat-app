c1955f61fbbdfb1f07d3e3ae62d9bea2c62af016
Gonzaga <luizgonzagabn@gmail.com>
2021-05-27 15:43:42 -0300

    commit firebase update config

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,28 +1,33 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, i) => Container(
-          padding: EdgeInsets.all(10),
-          child: Text('Funcionou!'),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection('chat')
-              .snapshots()
-              .listen((querySnapshot) {
-            print(querySnapshot.docs);
-          });
-        },
-      ),
-    );
+    return FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          return Scaffold(
+            body: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (ctx, i) => Container(
+                padding: EdgeInsets.all(10),
+                child: Text('Funcionou!'),
+              ),
+            ),
+            floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () {
+                FirebaseFirestore.instance
+                    .collection('chat')
+                    .snapshots()
+                    .listen((querySnapshot) {
+                  print(querySnapshot.docs);
+                });
+              },
+            ),
+          );
+        });
   }
 }
