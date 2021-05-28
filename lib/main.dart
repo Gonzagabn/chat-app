de5317ea23b3e56b584d094e8776cabf42d6b468
Gonzaga <luizgonzagabn@gmail.com>
2021-05-28 14:30:02 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,33 @@
 import 'package:chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Chat',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ChatScreen(),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        return MaterialApp(
+          title: 'Flutter Chat',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: ChatScreen(),
+        );
+      },
     );
   }
 }
