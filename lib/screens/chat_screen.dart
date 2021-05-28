ea9e8f8f22c7750bc2b7408161e9c7a60f68dba4
Gonzaga <luizgonzagabn@gmail.com>
2021-05-28 16:33:57 -0300

    commit firebase query fix

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,26 +1,31 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final Stream<QuerySnapshot> _chats =
+      FirebaseFirestore.instance.collection('chat').snapshots();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 8,
-        itemBuilder: (ctx, i) => Container(
-          padding: EdgeInsets.all(10),
-          child: Text('text'),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection('chat')
-              .snapshots()
-              .listen((event) {
-            print(event);
-          });
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _chats,
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          final documents = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, i) => Container(
+              padding: EdgeInsets.all(10),
+              child: Text(documents[i]['text']),
+            ),
+          );
         },
       ),
     );
