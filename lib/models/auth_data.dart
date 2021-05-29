2d2402b6e740f710d75ed0bc1cd25d7ac87c8c4e
Gonzaga <luizgonzagabn@gmail.com>
2021-05-29 14:23:15 -0300

    commit

---
+++ lib/models/auth_data.dart
@@ -0,0 +1,23 @@
+enum AuthMode {
+  LOGIN,
+  SIGNUP,
+}
+
+class AuthData {
+  String? name;
+  String? email;
+  String? password;
+  AuthMode _mode = AuthMode.LOGIN;
+
+  bool get isSignup {
+    return _mode == AuthMode.SIGNUP;
+  }
+
+  bool get isLogin {
+    return _mode == AuthMode.LOGIN;
+  }
+
+  void toggleMode() {
+    _mode = _mode == AuthMode.LOGIN ? AuthMode.SIGNUP : AuthMode.LOGIN;
+  }
+}
--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -1,3 +1,4 @@
+import 'package:chat/models/auth_data.dart';
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
@@ -6,6 +7,10 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final AuthData _authData = AuthData();
+
+  _submit() {}
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -17,30 +22,38 @@ class _AuthFormState extends State<AuthForm> {
             child: Form(
               child: Column(
                 children: <Widget>[
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Nome',
+                  if (_authData.isSignup)
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Nome',
+                      ),
+                      onChanged: (value) => _authData.name = value,
                     ),
-                  ),
                   TextFormField(
                     decoration: InputDecoration(
                       labelText: 'E-mail',
                     ),
+                    onChanged: (value) => _authData.email = value,
                   ),
                   TextFormField(
                     obscureText: true,
                     decoration: InputDecoration(
                       labelText: 'Senha',
                     ),
+                    onChanged: (value) => _authData.password = value,
                   ),
                   SizedBox(height: 12),
                   ElevatedButton(
-                    child: Text('Entrar'),
-                    onPressed: () {},
+                    child: Text(_authData.isLogin ? 'Entrar' : 'Cadastrar'),
+                    onPressed: _submit,
                   ),
                   TextButton(
                     child: Text('Criar uma nova conta?'),
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        _authData.toggleMode();
+                      });
+                    },
                   ),
                 ],
               ),
