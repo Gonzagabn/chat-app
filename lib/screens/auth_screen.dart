cd7c168e1b36f95bc24cc11f58ae5978a8598968
Gonzaga <luizgonzagabn@gmail.com>
2021-05-30 10:01:25 -0300

    commit

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,6 +1,8 @@
 import 'package:chat/models/auth_data.dart';
 import 'package:chat/widgets/auth_form.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AuthScreen extends StatefulWidget {
   @override
@@ -8,13 +10,44 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  void _handleSubmit(AuthData authData) {}
+  final _auth = FirebaseAuth.instance;
+  final GlobalKey<ScaffoldMessengerState> _scaffoldMessengerKey =
+      GlobalKey<ScaffoldMessengerState>();
+
+  Future<void> _handleSubmit(AuthData authData) async {
+    try {
+      if (authData.isLogin) {
+        await _auth.signInWithEmailAndPassword(
+          email: authData.email!.trim(),
+          password: authData.password!,
+        );
+      } else {
+        await _auth.createUserWithEmailAndPassword(
+          email: authData.email!.trim(),
+          password: authData.password!,
+        );
+      }
+    } on PlatformException catch (err) {
+      final msg = err.message ?? 'Ocorreu um erro! verifique suas credenciais.';
+      _scaffoldMessengerKey.currentState!.showSnackBar(
+        SnackBar(
+          content: Text(msg),
+          backgroundColor: Theme.of(context).errorColor,
+        ),
+      );
+    } catch (err) {
+      print(err);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      body: AuthForm(_handleSubmit),
+    return ScaffoldMessenger(
+      key: _scaffoldMessengerKey,
+      child: Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        body: AuthForm(_handleSubmit),
+      ),
     );
   }
 }
